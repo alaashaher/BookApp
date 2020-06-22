963a3cfea809e1e623f30902a5bbe17a5843cd9e
alaa shaher <alaashaher12@gmail.com>
2020-06-22 22:02:25 +0200

    create project

---
+++ Model/ApplictionDbContext.cs
@@ -0,0 +1,18 @@
+﻿using BookApp.wwwroot.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookApp.Model
+{
+    public class ApplictionDbContext : DbContext
+    {
+        public ApplictionDbContext(DbContextOptions<ApplictionDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Book> Book { get; set; }
+    }
+}
